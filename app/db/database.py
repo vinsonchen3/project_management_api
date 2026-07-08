6fd54a9c2b92e519996416b372c7d32e8b25a77e
vinsonchen3 <vinsonc0101@gmail.com>
2026-07-08 18:41:40 -0400

    initilialized sqllite database (switch to postgress after)

---
+++ app/db/database.py
@@ -0,0 +1,19 @@
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
+
+DATABASE_URL = "sqlite+aiosqlite:///./test.db"
+
+engine = create_async_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}, echo=False
+)
+
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine, class_=AsyncSession, expire_on_commit=False
+)
+
+class Base(DeclarativeBase):
+    pass
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
