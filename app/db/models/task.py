fd976c2b9685fcaf268a3f649f6dc3fd607aa09a
vinsonchen3 <vinsonc0101@gmail.com>
2026-07-09 17:03:58 -0400

    added in task model along with task and user association for many to many relationship, included this relationship in user model

--- app/db/models/task.py
+++ app/db/models/task.py
@@ -1,2 +1,38 @@
+from db.database import Base
+from db.models.user import User
 from datetime import datetime
+from sqlalchemy import (
+    String,
+    Integer,
+    Boolean,
+    DateTime,
+    Text,
+    ForeignKey,
+    Table,
+    Column,
+)
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
+user_task_association = Table(
+    "user_task",
+    Base.metadata,
+    Column(
+        "user_id", Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True
+    ),
+    Column(
+        "task_id", Integer, ForeignKey("tasks.id", ondelete="CASCADE"), primary_key=True
+    ),
+)
+
+
+class Task(Base):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    title: Mapped[str] = mapped_column(String(250), default="Untitiled")
+    description: Mapped[str] = mapped_column(Text)
+    # TODO: make status use enums for status codes insteaad of string
+    status: Mapped[str] = mapped_column(String, default="Not Started")
+
+    # relationships
+    assignees: Mapped[list[User]] = relationship(
+        User, secondary=user_task_association, back_populates="tasks"
+    )
--- app/db/models/user.py
+++ app/db/models/user.py
@@ -1,13 +1,19 @@
 from db.database import Base
+from db.models.task import Task, user_task_association
 from datetime import datetime
 from sqlalchemy import String, Integer, Boolean, DateTime
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 
 class User(Base):
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, index = True)
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
     email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
     hashed_password: Mapped[str] = mapped_column(String(200), nullable=False)
-    #relationships:
-    #tasks: Mapped[list[Task]]
+
+    # relationships
+    tasks: Mapped[list[Task]] = relationship(
+        Task, secondary=user_task_association, back_populates="assignees"
+    )
