dac12da33c11745c1432e9ed8e5a4253021dec53
vinsonchen3 <vinsonc0101@gmail.com>
2026-07-10 18:27:12 -0400

    added schema for users

---
+++ app/schemas/user.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from pydantic import BaseModel, ConfigDict, EmailStr, Field
+
+
+class UserBase(BaseModel):
+    username: str = Field(min_length=1, max_length=50, description="Unique username")
+    email: EmailStr = Field(max_length=100, description="Valid email address")
+
+class UserCreate(UserBase):
+    password: str = Field(min_length=8, description="Password; minimum 8 characters")
+
+class UserPublic(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    username: str
+    
