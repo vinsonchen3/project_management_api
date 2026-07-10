34a9965014efed66acb0a8f4b04e37b42052abe3
vinsonchen3 <vinsonc0101@gmail.com>
2026-07-10 19:16:16 -0400

    added comment pydantic schema

---
+++ app/schemas/comment.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from pydantic import BaseModel, ConfigDict, EmailStr, Field
+
+class CommentBase(BaseModel):
+    content: str = Field(min_length=1, description="Comment content")
+    
+class CommentCreate(CommentBase):
+    task_id: int 
+
+class CommentPublic(CommentBase):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    date_posted: datetime
+    author_id: int 
+    task_id: int
