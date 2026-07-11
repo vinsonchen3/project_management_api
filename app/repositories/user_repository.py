d9b19a4a132b22dbf7f2a96f5e32555a1ca30315
vinsonchen3 <vinsonc0101@gmail.com>
2026-07-11 16:44:49 -0400

    added all user repository layer methods

---
+++ app/repositories/user_repository.py
@@ -0,0 +1,56 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from db.models.user import User
+
+class UserRepository:
+    def __init__(self, db: AsyncSession):
+        self.db = db
+    
+    async def create(
+        self,
+        username: str,
+        email: str,
+        hashed_password
+    ) -> User:
+        user = User(username=username, email=email, hashed_password=hashed_password)
+        self.db.add(user)
+        await self.db.commit()
+        await self.db.refresh(user)
+        return user 
+
+    async def get_by_id(self, user_id: int)-> User | None:
+        result = await self.db.execute(select(User).where(User.id)==user_id)
+        return result.scalar_one_or_none()
+    
+    async def get_by_username(self, username: str) -> User | None:
+        result = await self.db.execute(
+            select(User).where(User.username == username)
+        )
+        return result.scalar_one_or_none()
+    
+    async def get_by_email(self, email: str) -> User | None:
+        result = await self.db.execute(
+            select(User).where(User.email == email)
+        )
+        return result.scalar_one_or_none()
+    
+    async def get_all(
+        self,
+        offset: int = 0,
+        limit: int = 100,
+    ) -> list[User]:
+        result = await self.db.execute(
+            select(User)
+            .offset(offset)
+            .limit(limit)
+        )
+        return result.scalars().all()
+    
+    async def update(self, user: User) -> User:
+        await self.db.commit()
+        await self.db.refresh(user)
+        return user
+    
+    async def delete(self, user: User) -> None:
+        await self.db.delete(user)
+        await self.db.commit()
