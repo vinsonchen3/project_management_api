896d75d75378f0ffdcac54cbdc2e536d4e4bcac4
vinsonchen3 <vinsonc0101@gmail.com>
2026-07-12 18:07:36 -0400

    create_db.py to test database connection and repo methods

---
+++ app/db/create_db.py
@@ -0,0 +1,16 @@
+from db.database import engine, Base
+
+from db.models.user import User
+from db.models.task import Task
+from db.models.comment import Comment
+
+
+async def main():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(main())
