19385c681fa4c7d0a254244434ee1223d079e4b9
vinsonchen3 <vinsonc0101@gmail.com>
2026-07-14 12:20:31 -0400

    added project relationships into user

--- app/db/models/user.py
+++ app/db/models/user.py
@@ -1,9 +1,10 @@
 from db.database import Base
-from db.models.task import user_task_association
+from db.models.task import user_task_association, Task
+from db.models.comment import Comment
+from db.models.project import Project, project_user_association
 from datetime import datetime
 from sqlalchemy import String, Integer, Boolean, DateTime
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-
 
 class User(Base):
     __tablename__ = "users"
@@ -19,4 +20,17 @@ class User(Base):
     )
     comments: Mapped[list["Comment"]] = relationship( # type: ignore
         back_populates="author", cascade="all, delete-orphan"
+    )
+
+    #projects owned by this user
+    owned_projects: Mapped[list["Project"]] = relationship(
+    back_populates="owner",
+    foreign_keys="Project.owner_id",
+    )
+
+    #projects this user is a member of 
+    projects: Mapped[list["Project"]] = relationship(
+        secondary=project_user_association,
+        back_populates="members",
+        lazy="selectin",
     )
