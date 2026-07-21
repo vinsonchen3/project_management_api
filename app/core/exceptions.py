1ea0b891afc45c4b44f83aaa7c6e445169e8d900
vinsonchen3 <vinsonc0101@gmail.com>
2026-07-21 16:22:36 -0400

    added custom exceptions and also created service layer files

---
+++ app/core/exceptions.py
@@ -0,0 +1,17 @@
+class UserNotFoundError(Exception):
+    pass
+
+class ProjectNotFoundError(Exception):
+    pass
+
+class TaskNotFoundError(Exception):
+    pass
+
+class CommentNotFoundError(Exception):
+    pass
+
+class DuplicateEmailError(Exception):
+    pass
+
+class DuplicateUsernameError(Exception):
+    pass
