4dde6c13cf442926bece3dc9d2a1fa6bb074cc7e
vinsonchen3 <vinsonc0101@gmail.com>
2026-07-21 14:11:06 -0400

    added in all relationships to user schema

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -6,12 +6,27 @@ class UserBase(BaseModel):
     username: str = Field(min_length=1, max_length=50, description="Unique username")
     email: EmailStr = Field(max_length=100, description="Valid email address")
 
+
 class UserCreate(UserBase):
     password: str = Field(min_length=8, description="Password; minimum 8 characters")
+
 
 class UserResponse(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
     id: int
     username: str
-    
+
+
+class UserDetailed(UserResponse):
+    owned_projects: list["ProjectResponse"] = []
+    projects: list["ProjectResponse"] = []
+    tasks: list["TaskResponse"] = []
+    comments: list["CommentResponse"] = []
+
+
+from schemas.project import ProjectResponse
+from schemas.task import TaskResponse
+from schemas.comment import CommentResponse
+
+UserDetailed.model_rebuild()
