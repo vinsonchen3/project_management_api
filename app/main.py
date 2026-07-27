e77fe19b35366c644267b11002d369b5f39ac675
vinsonchen3 <vinsonc0101@gmail.com>
2026-07-27 17:02:58 -0400

    put in a db connection pool ping at start of lifespan to ensure we connected to our db

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
-from fastapi import FastAPI
 from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+from sqlalchemy import text
+
+from app.db.database import engine
+
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     print("starting app")
+    async with engine.connect() as conn:
+        await conn.execute(text("SELECT 1"))
     yield
     print("shutting down app")
 
