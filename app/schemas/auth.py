cc1d13d1ba65b0ffd3044fbfe77f764c59494d29
vinsonchen3 <vinsonc0101@gmail.com>
2026-07-31 16:00:26 -0400

    added in loginrequest and token schemas

---
+++ app/schemas/auth.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel, EmailStr, Field
+
+class LoginRequest(BaseModel):
+    email: EmailStr
+    password: str
+
+class TokenResponse(BaseModel):
+    access_token: str 
+    token_type: str = "bearer"
