7b550b2296055f1d79d3cd98426937662ec06558
vinsonchen3 <vinsonc0101@gmail.com>
2026-08-06 22:47:17 -0400

    added all routers to main.py

--- app/main.py
+++ app/main.py
@@ -6,6 +6,9 @@ from sqlalchemy import text
 from app.db.database import engine
 from app.api.v1.auth import router as auth_router
 from app.api.v1.users import router as user_router
+from app.api.v1.projects import router as project_router
+from app.api.v1.tasks import router as task_router
+from app.api.v1.comments import router as comment_router
 
 
 @asynccontextmanager
@@ -22,6 +25,9 @@ app = FastAPI(lifespan=lifespan, title="Project Management API")
 # add routers here
 app.include_router(auth_router, prefix="/api/v1")
 app.include_router(user_router, prefix="/api/v1")
+app.include_router(project_router, prefix="/api/v1")
+app.include_router(task_router, prefix="/api/v1")
+app.include_router(comment_router, prefix="/api/v1")
 
 
 @app.get("/")
