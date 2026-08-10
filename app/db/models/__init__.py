7bf1b8d3a56eab43aa8f93c039256f03fa8bcdcd
vinsonchen3 <vinsonc0101@gmail.com>
2026-08-10 19:09:36 -0400

    added association tables to model init. set up alembic config

--- app/db/models/__init__.py
+++ app/db/models/__init__.py
@@ -1,6 +1,13 @@
 from app.db.models.user import User
-from app.db.models.task import Task
+from app.db.models.task import Task, user_task_association
 from app.db.models.comment import Comment
-from app.db.models.project import Project
+from app.db.models.project import Project, project_user_association
 
-__all__ = ["User", "Task", "Comment", "Project"]
+__all__ = [
+    "User",
+    "Task",
+    "Comment",
+    "Project",
+    user_task_association,
+    project_user_association,
+]
