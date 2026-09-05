ccb784086a78067a13647991ed47156ba0217047
vinsonchen3 <vinsonc0101@gmail.com>
2026-09-05 02:27:07 -0400

    made it run pytest as a module to fix imports

--- app/core/config.py
+++ app/core/config.py
@@ -3,7 +3,11 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore",
+    )
 
     # auth
     secret_key: SecretStr
@@ -16,10 +20,11 @@ class Settings(BaseSettings):
     database_url: str
     test_database_url: str | None = None
 
-import os
 
-print("SECRET_KEY exists:", "SECRET_KEY" in os.environ)
-print("ALGORITHM exists:", "ALGORITHM" in os.environ)
-print("PASSWORD_HASH_SCHEME exists:", "PASSWORD_HASH_SCHEME" in os.environ)
-print("DATABASE_URL exists:", "DATABASE_URL" in os.environ)
+# import os
+
+# print("SECRET_KEY exists:", "SECRET_KEY" in os.environ)
+# print("ALGORITHM exists:", "ALGORITHM" in os.environ)
+# print("PASSWORD_HASH_SCHEME exists:", "PASSWORD_HASH_SCHEME" in os.environ)
+# print("DATABASE_URL exists:", "DATABASE_URL" in os.environ)
 settings = Settings()
